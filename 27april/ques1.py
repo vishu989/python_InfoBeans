c83ec8b817dd14837994165fb880c1de0161c83e
vishu989 <vishalpatilvit2001@gmail.com>
2026-05-22 16:44:01 +0530

    Delete 27april directory

--- 27april/ques1.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a number: "))
-
-product = 1
-
-for i in range(1, n + 1):
-    if i % 2 != 0:   # check if number is odd
-        product *= i
-
-print(product)
--- 27april/ques2.py
+++
@@ -1,9 +0,0 @@
-a, b = map(int, input().split())
-
-count = 0
-
-for i in range(a, b+1):
-    if i % 7 == 0:
-        count += 1
-
-print("Count =", count)
